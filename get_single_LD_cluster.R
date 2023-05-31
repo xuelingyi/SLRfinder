9bff1cd5e80e73665129f672c2e2502f41abc896
Petri Kemppainen <p3taimen@gmail.com>
2023-05-31 21:44:54 +0800

    Update get_single_LD_cluster.R

--- get_single_LD_cluster.R
+++ get_single_LD_cluster.R
@@ -2,6 +2,8 @@
 
 # NOTE: LD.data is the full name (including path) of the *.geno.ld file putput from vcftools 
 # NOTE: the function works on each chr separately 
+ library("igraph")
+ library(data.table)
 
 get_single_LD_cluster <- function(LD.data, min_LD = 0.9, min.cl.size=10){
   
@@ -12,10 +14,10 @@ get_single_LD_cluster <- function(LD.data, min_LD = 0.9, min.cl.size=10){
   cat("Removing edges data lower than min_LD \n")
   white.list <- subset(geno.LD, as.numeric(r2)>min_LD)
   
-  library("igraph")
+ 
   cat("Parsing to graph object \n")
   g <- graph_from_edgelist(apply(white.list[,c("from", "to")], 2, function(o) as.character(o)), directed = FALSE)
-  E(g)$weight <- as.numeric(white.list[,"r2"])
+
   cat("Decomposing graph \n")
   d_g <- decompose.graph(g)
   d_g <- d_g[sapply(d_g, length)>min.cl.size]
@@ -23,7 +25,7 @@ get_single_LD_cluster <- function(LD.data, min_LD = 0.9, min.cl.size=10){
   #length(d_g)
   
   cat("Summerizing data \n")
-  library(data.table)
+ 
   out <- rbindlist(lapply(d_g, function(g){
     nSNPs <- length(g)
     mean_LD <- mean(as.numeric(white.list[white.list$from %in% g & white.list$to %in% g,"r2"]))
