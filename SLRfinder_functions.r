db94afb51632473f77891bd6f04421a19cf5c988
Xueling Yi <willyxl3@163.com>
2023-10-31 14:32:53 +0800

    Update SLRfinder_functions.r

--- SLRfinder_functions.r
+++ SLRfinder_functions.r
@@ -131,7 +131,8 @@ get_candidate_regions <- function(data_cls, GT, map, pop, ranks=c("Dext_max_rank
   candidates <- data_out[which(p_gc_adj<alpha),]
   if(nrow(candidates) == 0){
     print("No candidates found! Report the top 5 ranked LD clusters.")
-    candidates <- data_out[1:5,]    
+    if(nrow(data_out) < 5){ candidates <- data_out  }
+    if(nrow(data_out) >= 5){ candidates <- data_out[1:5,] } 
   } 
   
   myindex = length(grep("_", candidates[1, "chr"])) + 2
