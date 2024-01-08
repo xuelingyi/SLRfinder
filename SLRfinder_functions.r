80d9bd8bbe0058e9f7ca9299383d334ab261dba2
Xueling Yi <willyxl3@163.com>
2024-01-08 14:00:56 +0800

    Update SLRfinder_functions.r

--- SLRfinder_functions.r
+++ SLRfinder_functions.r
@@ -42,7 +42,8 @@ get_single_LD_cluster <- function(geno.LD, min_LD = 0.85, min.cl.size = 20){
 
 eucl <- function(i, j){ sqrt((i[,1]-j[1])^2 + (i[,2]-j[2])^2 ) }
 
-get_candidate_regions <- function(data_cls, GT, map, pop, ranks=c("Dext_max_rank","R2_rank","nSNPs_rank","chi2_rank"), nPerm=10000, cores=1, alpha=0.05){
+
+get_data_output = function(data_cls, GT, map, pop, sex_info=T, cores=1){
   
   cat("Generating gds file \n")
   name <- paste0("file.gds")
@@ -50,11 +51,13 @@ get_candidate_regions <- function(data_cls, GT, map, pop, ranks=c("Dext_max_rank
   file_gds <- snpgdsOpen(name)
   
   cat("Processing data \n")
-  data_out <-  rbindlist(mclapply(1:nrow(data_cls), function(cl){
+  data_out <- rbindlist(mclapply(1:nrow(data_cls), function(cl){
     cl_info <- data_cls[cl,]
     SNPs <- cl_info$SNPs[[1]]
-    pca <- snpgdsPCA(file_gds,snp.id = cl_info$SNPs[[1]],verbose = FALSE)
     gt <- GT[,which(map$SNP %in% SNPs)]
+    
+    #if(nrow(gt) > 0){}
+    pca <- snpgdsPCA(file_gds, snp.id = cl_info$SNPs[[1]], verbose = FALSE)
     PC1 <- as.matrix(pca$eigenvect[,1])
     PVE <- pca$eigenval[1]/sum(na.omit(pca$eigenval))
     PC2 <- as.matrix(pca$eigenvect[,2])
@@ -67,6 +70,8 @@ get_candidate_regions <- function(data_cls, GT, map, pop, ranks=c("Dext_max_rank
     if(!(is.na(my.cor)) & my.cor<0) PC1 <- -PC1   
     data <- data.table(PC1=as.vector(PC1),Het=het)
     data$PC2 = PC2
+    
+    ## Note: individuals having only missing data would have het == NaN, which would be discarded in R2 estimations
     R2 <- summary(lm(PC1~Het,data))$adj.r.squared
     cl_info[,R2:=R2]
     cl_info[,PVE:=PVE]
@@ -75,14 +80,19 @@ get_candidate_regions <- function(data_cls, GT, map, pop, ranks=c("Dext_max_rank
     data[,PC_scaled:=(PC1-min(PC1))/max((PC1-min(PC1)))]
     
     # get all distances from the corners
-    c1 <- c(PC1=0,het=0) ## bottom left corner
-    c2 <- c(PC1=1,het=1) ## top right corner
-    Dists <-  cbind(d_c1=eucl(data[,.(PC_scaled,Het)],c1),d_c2=eucl(data[,.(PC_scaled,Het)],c2))
-    # divide the minimum with the maximum possible so values are scaled betweeen 0 and 1
+    ## bottom left corner
+    c1 <- c(min(data[data$Het == min(na.omit(data$Het)), "PC_scaled"]), min(na.omit(data$Het))) 
+    ## top right corner
+    c2 <- c(max(data[data$Het == max(na.omit(data$Het)), "PC_scaled"]), max(na.omit(data$Het)))  
+    d_c1=eucl(data[,.(PC_scaled,Het)],c1)
+    d_c2=eucl(data[,.(PC_scaled,Het)],c2)
+    Dists <-  cbind(d_c1, d_c2)
+    #standarize distances for between LD comparisons 
     Dext <- apply(Dists,1,min)/((eucl(matrix(c1,nrow=1),c2))/2)
-    
-    cl_info[,Dext_mean:=mean(Dext)] # mean
-    cl_info[,Dext_max:=max(Dext)] # max
+    cl_info[,Dext_mean:=mean(Dext[!is.na(Dext)])] # mean
+    cl_info[,Dext_max:=max(Dext[!is.na(Dext)])] # max
+    cl_info[,Dext_median:=median(Dext[!is.na(Dext)])] # median, accouting for outliers (may be true due to sampling error etc.)
+    cl_info[,Dext_var:=var(Dext[!is.na(Dext)])]
     
     data[,Ind:=ind]
     data[,Pop:=pop]
@@ -91,15 +101,48 @@ get_candidate_regions <- function(data_cls, GT, map, pop, ranks=c("Dext_max_rank
       chisq.test(x)$statistic
     }))] # max
     
-    return(cl_info[, .(chr, nSNPs, mean_LD, nE, c, R2, PVE, PVE2, Dext_mean, Dext_max, chi2, SNPs, data=list(data))])
+    cl_info[,Sex_g:=1]
+    if(sex_info){
+      data = merge(data, sif[, c("SampleID", "sex")], by.x="Ind", by.y="SampleID", sort=F)
+      data_sub = data[data$sex %in% c("female", "Female", "F", "male", "Male", "M"), ]
+      cl_info[,Sex_g:=(min(
+        sum(data_sub[data_sub$PC_scaled < 0.5, "sex"][[1]] %in% c("female", "Female", "F")), 
+        sum(data_sub[data_sub$PC_scaled < 0.5, "sex"][[1]] %in% c("male", "Male", "M"))
+        ) + 
+          min(
+            sum(data_sub[data_sub$PC_scaled >= 0.5, "sex"][[1]] %in% c("female", "Female", "F")),
+            sum(data_sub[data_sub$PC_scaled >= 0.5, "sex"][[1]] %in% c("male", "Male", "M")))/nrow(data_sub))]}
     
+    return(as.data.frame(cl_info[, .(chr, nSNPs, mean_LD, nE, c, R2, PVE, PVE2, Dext_mean, Dext_max, Dext_median, Dext_var, Sex_g, chi2, SNPs, data=list(data))]))
   },mc.cores=cores))
   
+  cat("Closing gds file and returning data \n\n")
+  snpgdsClose(file_gds)
+  system("rm file.gds")
+  
+  # cluster ID
   data_out[,cluster:=1:nrow(data_out)]
-  data_out[order(nSNPs,decreasing = TRUE),nSNPs_rank := 1:nrow(data_out)]
-  data_out[order(R2,decreasing = TRUE),R2_rank := 1:nrow(data_out)]
-  data_out[order(Dext_max,decreasing = FALSE),Dext_max_rank := 1:nrow(data_out)]
-  data_out[order(chi2,decreasing = FALSE),chi2_rank := 1:nrow(data_out)]
+  
+  # rank clusters: use the same rank when having the same values
+  data_out$Dext_mean_rank=rank(data_out$Dext_mean, ties.method="min")
+  data_out$Dext_median_rank=rank(data_out$Dext_median, ties.method="min")
+  data_out$Dext_max_rank=rank(data_out$Dext_max, ties.method="min")
+  data_out$Dext_var_rank=rank(data_out$Dext_var, ties.method="min")
+  data_out$chi2_rank=rank(data_out$chi2, ties.method="min")
+  data_out$Sex_rank=0
+  if(sex_info){
+    data_out$Sex_rank=rank(data_out$Sex_g, ties.method="min")
+  } 
+  # rank by decreasing order!
+  data_out$nSNPs_rank=rank(-data_out$nSNPs, ties.method="min")
+  data_out$R2_rank=rank(-data_out$R2, ties.method="min")
+  return(data_out)
+}
+
+
+
+get_candidate_regions <- function(data_out, ranks=c("Dext_var_rank", "R2_rank","nSNPs_rank","chi2_rank", "Sex_rank"), nPerm=10000, cores=1, alpha=0.05){
+  
   rank=rowSums(data_out[,..ranks])
   data_out[,rank:=rank]
   setorder(data_out,rank)
@@ -119,7 +162,6 @@ get_candidate_regions <- function(data_cls, GT, map, pop, ranks=c("Dext_max_rank
   qq_data <- data.table(exp=-log10(ppoints(p)),obs=-log10(sort(p)))
   lambda <- lm(obs~exp+0,qq_data)$coefficients
   p_gc <- 1/10^(-log10(p)/lambda)
-  
   data_out[,p:=p]
   data_out[,p_gc:=p_gc]
   data_out[,p_gc_adj:=p.adjust(p_gc,"fdr")]
@@ -146,8 +188,14 @@ get_candidate_regions <- function(data_cls, GT, map, pop, ranks=c("Dext_max_rank
   
   candidates[,region:=apply(candidates,1,function(x)region=paste(x$chr, paste(range(as.numeric(do.call(rbind,strsplit(x$SNPs,"_",fixed=TRUE))[,myindex])),collapse="-"),sep=":"))]
   
-  cat("Closing gds file and returning data \n\n")
-  snpgdsClose(file_gds)
-  system("rm file.gds")
+  print(paste0(nrow(candidates), " candidate(s)"))
+  candidates = merge(candidates, LG, by="chr")
+  
+  write.csv(candidates[, c("chr", "lg", "region", "rank", "p_gc_adj",
+                           "nSNPs", "Dext_mean", "R2", "chi2", "Sex_g",
+                           "nSNPs_rank", "Dext_mean_rank", "R2_rank", "chi2_rank", "Sex_rank", 
+                           "Dext_median_rank", "Dext_max_rank", "mean_LD", "Dext_max")], 
+            "candidates.csv", row.names = F)
+  
   return(list(data_out=data_out, candidates=candidates, qq_data=qq_data, PCA_het_data=PCA_het_data, lambda=lambda))
 }
